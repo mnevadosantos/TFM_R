c2349210d9f55becdadb02a0e434f8ce0f435d3c
mnevadosantos <mnevadosantos@gmail.com>
2022-03-27 22:11:55 +0200

    Uploading a cleaning and exploring script to choose variables for the study

---
+++ Scripts/4_cleaningDataVariables.R
@@ -0,0 +1,49 @@
+#########################################################################################
+## This script explores and cleans the variables of the dataset resulting for the       #
+## previous script, 3_clinGenData.R.                                                    #
+#########################################################################################
+
+# First, loading the dataset:
+datos_global <- read.csv("../Data/datos_global.csv", sep = ";", header = T, stringsAsFactors = T)
+
+# The columnos corresponding to the variants are:
+grep("^X+", names(datos_global)) # They are the columns 14 to 172
+
+# Now exploring some variables:
+nrow(datos_global[is.na(datos_global$vital_status),])
+nrow(datos_global[datos_global$vital_status != "Alive",]) # Number of death patients
+as.integer(datos_global$death_days_to[datos_global$vital_status != "Alive"]) # Survivance (days) of death patients
+min(as.integer(datos_global$death_days_to[datos_global$vital_status != "Alive"]))
+max(as.integer(datos_global$death_days_to[datos_global$vital_status != "Alive"]))
+
+# Exploring causes of death:
+summary(datos_global$cause_of_death)
+datos_global$cause_of_death_source[datos_global$vital_status != "Alive"] # Uninteresting variable
+datos_global$cause_of_death_source <- NULL # Deleting
+
+# Exploring tumor status:
+levels(datos_global$tumor_status)
+summary(datos_global$tumor_status) # There is 86 NAs, and 5 Discrepancy
+
+# Exploring adjuvancy with radiotherapy
+summary(datos_global$radiation_treatment_adjuvant) # 209 NAs
+
+# Exploring chemotherapic adjuvancy
+summary(datos_global$pharmaceutical_tx_adjuvant) # 208 NAs
+
+# Selecting clinical variables by NAs proportion (<= 10%:
+clinical_var_index <- c(4:14, 175:245) # Clinical variables
+sel_clinical_var <- c()
+sel_clinical_index <- c()
+for (index in clinical_var_index) {
+  nas <- nrow(datos_global[is.na(datos_global[index]),])
+  if (nas < 50){
+    sel_clinical_var <- c(sel_clinical_var, colnames(datos_global[index]))
+    sel_clinical_index <- c(sel_clinical_index, index)
+  }
+}
+# Studying the results:
+length(sel_clinical_var)
+sapply(sel_clinical_index, function(x){
+  summary(datos_global[x])
+})
