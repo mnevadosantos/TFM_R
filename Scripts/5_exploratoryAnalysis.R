1fd4043c895eb269e75fb04e9b105e3e39f5b9e8
mnevadosantos <mnevadosantos@gmail.com>
2022-04-10 00:22:16 +0200

    Exploratory analysis ended.

--- Scripts/5_exploratoryAnalysis.R
+++ Scripts/5_exploratoryAnalysis.R
@@ -35,3 +35,37 @@ hist(who,
      xlab = "WHO's grade group", 
      ylab = "Frequency", 
      main = "Histogram of WHO's grade groups")
+
+summary(laterality)
+summary(history_other_malignancy)
+summary(race)
+levels(race)
+datos_global$race[datos_global$race == "[Not Evaluated]"] <- NA
+
+summary(age_at_initial_pathologic_diagnosis)
+nrow(datos_global[is.na(age_at_initial_pathologic_diagnosis), ])
+mfv(age_at_initial_pathologic_diagnosis)
+var(age_at_initial_pathologic_diagnosis)
+sd(age_at_initial_pathologic_diagnosis)
+skewness(age_at_initial_pathologic_diagnosis)
+kurtosis(age_at_initial_pathologic_diagnosis)
+hist(age_at_initial_pathologic_diagnosis,  
+     col = "darkgreen",
+     xlab = "Age at initial pathologic diagosis", 
+     ylab = "Frequency", 
+     main = "Histogram of age at diagnosis")
+
+summary(clinical_stage)
+
+summary(pathologic_T)
+barplot(table(pathologic_T), 
+        col = "darkgreen", 
+        xlab = "Pathologic T",
+        ylab = "Frequency", 
+        main = "Pathologic T")
+
+summary(clinical_N)
+summary(clinical_M)
+
+# Initial study will include The age at initial histologic diagnosis, the Gleason score,
+# the WHO's grade group and the pathologic T.
